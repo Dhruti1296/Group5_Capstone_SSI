d045c5cfc116c819d2dca053400dd30c6f14e8e2
Christ Vijay S <christvijay1998@gmail.com>
2026-02-10 14:01:38 -0500

    Sprint1-Backend Authentication, User, Admin Registration Changed

---
+++ backend/SSI.API/SSI.API/Data/MongoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using SSI.API.Models;
+
+namespace SSI.API.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration config)
+        {
+            var client = new MongoClient(config["MongoDbSettings:ConnectionString"]);
+            _database = client.GetDatabase(config["MongoDbSettings:DatabaseName"]);
+        }
+        // users
+        public IMongoCollection<User> Users =>
+            _database.GetCollection<User>("Users");
+
+        //Admin
+        public IMongoCollection<Admin> Admins =>
+    _database.GetCollection<Admin>("Admins");
+    }
+}
