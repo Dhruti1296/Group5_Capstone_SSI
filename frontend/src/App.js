53bf305644c27a1c636af641f412f79bf536ed99
Aakash <aakashnair19@gmail.com>
2026-02-22 13:50:35 -0500

    app.js changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import SideMenu from "./components/SideMenu";
+import VideoBackground from "./components/VideoBackground";
+import AlumniList from "./components/AlumniList";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar onMenuClick={() => setMenuOpen(true)} />
+      <VideoBackground />
+      
+      {/* Side Menu */}
+      {menuOpen && <SideMenu onClose={() => setMenuOpen(false)} />}
+
+      {/* Alumni Cards */}
+      <AlumniList />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
