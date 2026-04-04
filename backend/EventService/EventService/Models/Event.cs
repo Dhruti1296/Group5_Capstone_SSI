6641aa1e6cb80811c8de20fa5da1ff90dd2f7a66
Vijay <81905551+Christ-vijay@users.noreply.github.com>
2026-04-04 15:04:38 -0400

    Event Management Micro Services added

---
+++ backend/EventService/EventService/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace EventService.Models
+{
+    public class Event
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; } //type of event
+        public DateTime EventDate { get; set; }
+        public string? Location { get; set; }
+        [JsonIgnore]
+        public string? ImageUrl { get; set; }
+    }
+}
