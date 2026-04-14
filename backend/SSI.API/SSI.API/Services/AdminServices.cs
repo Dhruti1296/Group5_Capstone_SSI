0075f8b5bdacc76a4fa464d5f69c078102bf9eb0
Teena M.T <teena.thomas118@gmail.com>
2026-04-14 09:04:10 -0400

    Added comments and refined admin service logic

--- backend/SSI.API/SSI.API/Services/AdminServices.cs
+++ backend/SSI.API/SSI.API/Services/AdminServices.cs
@@ -6,19 +6,23 @@ namespace SSI.API.Services
 {
     public class AdminServices
     {
+        // MongoDB collection for Admin...
         private readonly IMongoCollection<Admin> _admins;
 
+        // constructor to initialize MongoDB collection using context..
         public AdminServices(MongoDbContext context)
         {
             _admins = context.Admins;
         }
 
+        // retrieve username and role based on userName...
         public async Task<Admin?> GetByUserName(string userName)
         {
             return await _admins
                 .Find(a => a.UserName == userName)
                 .FirstOrDefaultAsync();
         }
+
 
         public async Task CreateAdmin(Admin admin)
         {
